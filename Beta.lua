4d034ae14e06269be0b2d28b18b25bcd22c0fceb
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-02 16:17:51 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -3117,34 +3117,38 @@ local function AutoBuyCapsuleFunc ()
 
 	if Candy <= 149 then return end
 
-    local args
-    if Candy >= 15000 then
-        args = {
-            [1] = "capsule_halloween2",
-            [2] = "event",
-            [3] = "event_shop",
-            [4] = "100"
-        }
-    elseif Candy >= 1500 then
-        args = {
-            [1] = "capsule_halloween2",
-            [2] = "event",
-            [3] = "event_shop",
-            [4] = "10"
-        }
-    elseif Candy >= 150 then
-        args = {
-            [1] = "capsule_halloween2",
-            [2] = "event",
-            [3] = "event_shop",
-            [4] = "1"
-        }
-    end
+	while GetSave(AutoBuyCapsule.Name)
+		local args
+		if Candy >= 15000 then
+			args = {
+				[1] = "capsule_halloween2",
+				[2] = "event",
+				[3] = "event_shop",
+				[4] = "100"
+			}
+		elseif Candy >= 1500 then
+			args = {
+				[1] = "capsule_halloween2",
+				[2] = "event",
+				[3] = "event_shop",
+				[4] = "10"
+			}
+		elseif Candy >= 150 then
+			args = {
+				[1] = "capsule_halloween2",
+				[2] = "event",
+				[3] = "event_shop",
+				[4] = "1"
+			}
+		end
+	
+		if args then
+			game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_item_generic:InvokeServer(unpack(args))
 
-    if args then
-        game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_item_generic:InvokeServer(unpack(args))
-    end
-end	
+		task.wait(0.05)
+		end
+	end
+	
 
 DDLlabel(selectedSkinsDDL, GetSave('Delete Skins'))
 
