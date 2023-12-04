a1c51ab63748ea6da2da6df555ee1c8e24704046
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-04 20:54:52 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -4477,6 +4477,8 @@ local function webhook ()
 		if newResourcesTable.amount <=0 then continue end
 		newItems = newItems .. string.format("+%s %s\n", math.floor(newResourcesTable.amount), newResourcesTable.name)
 	end
+	
+	local totalItems = newResourcesTable[itemName].amount or 0
 
 	local currentItemCount = 0
 	for itemName, itemAmount in pairs(newItemsTable) do
@@ -4484,7 +4486,7 @@ local function webhook ()
 		local comma = ""
 		if currentItemCount ~= newItemsCount then comma = "\n" end
 	
-		newItems = newItems .. string.format('+%s (%s), Total: (x%s)', itemName, itemAmount, newResourcesTable[itemName].amount) .. comma
+		newItems = newItems .. string.format('+%s (%s), Total: (x%s)', itemName, itemAmount, totalItems) .. comma
 	end
 
 	if not ping then userID = "" end
