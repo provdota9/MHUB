1edbcf0a846564f7e9a5e92f2eb36def9a6fe8c6
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-05 00:02:29 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -4478,11 +4478,11 @@ local function webhook ()
 		newItems = newItems .. string.format("+%s %s\n", math.floor(newResourcesTable.amount), newResourcesTable.name)
 	end
 
+	local itemTotal = itemData.Amount
 	local currentItemCount = 0
-	for itemName, itemAmount in pairs(newItemsTable) do
+	for itemName, itemAmount, itemTotal in pairs(newItemsTable) do
 		currentItemCount = currentItemCount + 1
 		local comma = ""
-		local itemTotal = Data.Amount
 		if currentItemCount ~= newItemsCount then comma = "\n" end
 	
 		newItems = newItems .. string.format('+%s (%s), Total: (%s)', itemName, itemAmount, itemTotal) .. comma
