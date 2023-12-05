f3fff4a4bdba92fa3b46b99852f5a7624cf97a41
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-05 13:41:09 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -4482,8 +4482,8 @@ local function webhook ()
 		local currentItemCount = 0
 		for itemName, itemAmount in pairs(newItemsTable) do
 			currentItemCount = currentItemCount + 1
-			local comma = ""
-			if currentItemCount ~= newItemsCount then comma = "\n" end
+			local comma = "\n"
+			if currentItemCount ~= newItemsCount end
 	
 			if itemData.Name == itemName then
 				newItems = newItems .. string.format("+%s (%s), Total: (x%s) ", itemName, itemAmount, itemData.Amount) .. comma
