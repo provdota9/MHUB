851ed034f6bcbdf305b4d72e9406d1e97adc851e
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-10 17:05:25 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -1706,11 +1706,11 @@ MakeTitle(Other_MiscSubPage, 'Other', 0.16)
 local RenderingOff = MakeCheckbox(Other_MiscSubPage, 'Disable 3d Rendering', 0.13)
 local creditsButton = MakeLargeButton(Other_MiscSubPage, 'Copy Discord link', 0.155)
 
-local FakeStats_MiscSubPage = MakeNewSubPage('Misc', 'Left', 0.532, 0.1, 0.02, 0.05)
+local FakeStats_MiscSubPage = MakeNewSubPage('Misc', 'Left', 0.532, 0.08, 0.02, 0.05)
 MakeTitle(FakeStats_MiscSubPage, 'Fake Stats & Traits', 0.082)
-local AttackTextBox = MakeTextBox(FakeStats_MiscSubPage, 'Attack %', 'Attack', 0.26)
-local spaTextBox = MakeTextBox(FakeStats_MiscSubPage, 'SPA %', 'SPA', 0.26)
-local rangeTextBox = MakeTextBox(FakeStats_MiscSubPage, 'Range %', 'Range', 0.26)
+local AttackTextBox = MakeTextBox(FakeStats_MiscSubPage, 'Attack %', 'Attack', 0.17)
+local spaTextBox = MakeTextBox(FakeStats_MiscSubPage, 'SPA %', 'SPA', 0.17)
+local rangeTextBox = MakeTextBox(FakeStats_MiscSubPage, 'Range %', 'Range', 0.17)
 local FakeStatsButton = MakeLargeButton(FakeStats_MiscSubPage, 'Use Fake Stats', 0.155)
 
 ---------------------------------------------------------------------
