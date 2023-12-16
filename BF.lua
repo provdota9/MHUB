a0717e89d1f3b50255095bc7412072928286f5be
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-16 21:24:58 +0700

    Update BF.lua

--- BF.lua
+++ BF.lua
@@ -1073,11 +1073,11 @@ Pages['Main'].BottomLine.Visible = true
 local Main_MainSubPage = MakeNewSubPage('Main', 'Right', 0.603, 0.03, 0.01, 0.02)
 MakeTitle(Main_MainSubPage, 'Main', 0.07)
 local AutoRaceABL = MakeCheckbox(Main_MainSubPage, 'Auto Race Ability', 0.056)
-local AutoRaceAWK = MakeCheckbox(Main_MainSubPage, 'Auto V4 (ability)', 0.056)
+local AutoRaceAWK = MakeCheckbox(Main_MainSubPage, 'Auto V4 Ability', 0.056)
 
 ----------------------------------------------FUNCTIONS-------------------------------------
 
-local funtion AutoRaceAWKFunc()
+local function AutoRaceAWKFunc()
     while GetSave(AutoAWK.Name) do
         local args = {
             [1] = true
