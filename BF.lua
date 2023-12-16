2877b79b1c1f0d621b0a065425b1910d02c289b8
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-16 21:55:11 +0700

    Update BF.lua

--- BF.lua
+++ BF.lua
@@ -1066,6 +1066,7 @@ local Main_MainSubPage = MakeNewSubPage('Main', 'Right', 0.603, 0.03, 0.01, 0.02
 MakeTitle(Main_MainSubPage, 'Main', 0.07)
 local AutoRaceAWK = MakeCheckbox(Main_MainSubPage, 'Auto V4 Ability', 0.056)
 local AutoRaceABL = MakeCheckbox(Main_MainSubPage, 'Auto Race Ability', 0.056)
+local TPToLobby = MakeLargeButton(Main_MainSubPage, 'Server Hop', 0.103)
 
 ----------------------------------------------FUNCTIONS-------------------------------------
 
