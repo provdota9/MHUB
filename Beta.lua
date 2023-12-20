fff5ea8c9be8d571c672635ed454244a163b49d7
provdota9 <152515837+provdota9@users.noreply.github.com>
2023-12-20 22:54:39 +0700

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -1911,7 +1911,7 @@ Raid_SelectWorld.MouseButton1Click:Connect(function()
 		if not worldModule:IsA('ModuleScript') then continue end
 
 		for _,worldAbout in pairs(require(worldModule)) do
-			if worldAbout.name ~= 'Bizzare Town' and worldAbout.name ~= 'Sacred Planet' then continue end
+			if worldAbout.name ~= 'Morioh' and worldAbout.name ~= 'Sacred Planet' then continue end
 
 			local worldName = worldAbout.name
 
